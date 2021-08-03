75b01395699ebbf1ae5f04d65566ed5aa97fbecc
Amnah <aminahahmed1999@gmail.com>
2021-08-03 12:04:09 +0300

    Creation Building, Floor, and User classes in DB project.

---
+++ MaintenanceMagementSystems.Database/Models/Building.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintenanceManagementSystem.Database.Models
+{
+    public class Building
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public char Number { get; set; }
+
+        [Required]
+        public bool IsOwned { get; set; }
+
+        [Required]
+        [ForeignKey("Id")]
+        public int CountryId { get; set; }
+
+        [Required]
+        [ForeignKey("Id")]
+        public int CityId { get; set; }
+
+        [Required]
+        public string Street { get; set; }
+    }
+}
---
+++ MaintenanceMagementSystems.Database/Models/Floor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintenanceManagementSystem.Database.Models
+{
+    public class Floor
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Key]
+        [Required]
+        public char Number { get; set; }
+
+        [ForeignKey("Id")]
+        [Required]
+        public int BuildingId { get; set; }
+    }
+}
