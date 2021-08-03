7cf901b257cb2afa8020cc63dc6b5053c05817e3
Amnah <aminahahmed1999@gmail.com>
2021-08-03 16:31:15 +0300

    Creation of Interfaces

---
+++ MaintenanceMagementSystems.Application/Interfaces/IBeneficiary.cs
@@ -0,0 +1,18 @@
+﻿using MaintenanceManagementSystem.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintenanceManagementSystem.Application.Interfaces
+{
+    public interface IBeneficiary
+    {
+        public bool Register(User user);
+        public bool CheckExistence(string email, string password); //for login
+        public bool ForgetPassword(string email);
+        public bool ChangePassword(string password);
+        public void ChangeLanguage(); //?????????????????
+    }
+}
