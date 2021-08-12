e0c6b87ccbcd6177705f4a3f3f3a767a8e2d638f
Amnah <aminahahmed1999@gmail.com>
2021-08-12 15:09:44 +0300

    Maintenece Manager & Maintenece Worker API

--- MaintenanceMagementSystems.API/Controllers/MaintenanceWorkerController.cs
+++ MaintenanceMagementSystems.API/Controllers/MaintenanceWorkerController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MaintenanceManagementSystem.Application.Interfaces;
+using MaintenanceManagementSystem.Database.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +13,41 @@ namespace MaintenanceManagementSystem.API.Controllers
     [ApiController]
     public class MaintenanceWorkerController : ControllerBase
     {
+        private readonly IMaintenanceWorker _maintenanceWorker;
+        public MaintenanceWorkerController(IMaintenanceWorker maintenanceWoker)
+        {
+            _maintenanceWorker = maintenanceWoker;
+        }
+
+
+        [HttpGet]
+        [Route("GetTicket/{id}")]
+        public IActionResult GetTicket(int TicketId)
+        {
+            Ticket ticket = _maintenanceWorker.GetTicket(TicketId);
+            return Ok(ticket);
+        }
+
+        //--------------------------------------------------------------------------------------------
+
+        [HttpGet]
+        [Route("ViewTickets")]
+        public IActionResult ListTickets()
+        {
+            List<Ticket> TicketList = _maintenanceWorker.ListTickets();
+            return Ok(TicketList);
+
+        }
+
+        //--------------------------------------------------------------------------------------------
+
+        [HttpPost]
+        [Route("AcceptingTicket")]
+        public IActionResult AcceptingTicket(int TicketId)
+        {
+             _maintenanceWorker.AcceptingTicket(TicketId);
+            return Ok();
+
+        }
     }
 }
