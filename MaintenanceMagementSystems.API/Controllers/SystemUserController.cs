0562784205a1379911365bd018320d9b94e3ad8c
Amnah <aminahahmed1999@gmail.com>
2021-08-18 11:39:42 +0300

    email

--- MaintenanceMagementSystems.API/Controllers/SystemUserController.cs
+++ MaintenanceMagementSystems.API/Controllers/SystemUserController.cs
@@ -35,25 +35,7 @@ namespace MaintenanceManagementSystem.API.Controllers
                 _systemUser.UpdateUser(user);
                 return Ok();
             }
-        //--------------------------------------------------------------------------------------------
-        [HttpPost]
-        [Route("[action]")]
-        [ActionName("SendEmail/{email}")]
-        public IActionResult SendEmail(string email)
-        {
-            _systemUser.SendEmail(email);
-            return Ok();
-        }
-        //--------------------------------------------------------------------------------------------
-        [HttpPost]
-        [Route("[action]")]
-        [ActionName("ResetPassword/{tempPassword}/{newPassword}")]
-        public IActionResult ResetPassword(string tempPassword, string newPassword)
-        {
-            _systemUser.ResetPassword(tempPassword, newPassword);
-            return Ok();
-        }
-        //--------------------------------------------------------------------------------------------
 
+            //--------------------------------------------------------------------------------------------
     }
 }
